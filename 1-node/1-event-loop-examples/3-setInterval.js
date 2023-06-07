8abba67fc1a12bcc0f2be0dc8d7c89ed4d91e70f
wolfgunblood <jalajdorai1@gmail.com>
2023-06-07 08:03:56 +0530

    setinterval

---
+++ 1-node/1-event-loop-examples/3-setInterval.js
@@ -0,0 +1,10 @@
+console.log("start")
+
+setInterval(() => {
+    console.log("hello world")
+}, 2000);
+
+console.log(`I will run first`)
+// process stays alive unless
+// Kill Process CONTROL + C
+// unexpected error
