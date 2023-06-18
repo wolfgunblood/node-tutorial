7b8bd0e7e5a440ff4132564330d021aeae4ccd6c
wolfgunblood <jalajdorai1@gmail.com>
2023-06-18 16:55:14 +0530

    Basic Json

--- 2-express/final/05-all-static.js
+++ 2-express/final/05-all-static.js
@@ -3,12 +3,15 @@ const path = require('path');
 const app = express();
 
 //middlewae to add js and css files to the html
+
 app.use(express.static('./public'))
 
 //all from static
 // app.get('/', (req, res) => {
 //     res.sendFile(path.join(__dirname, './public/index.html'));
 // })
+
+
 
 app.all('*', (req, res) => {
     res.status(404).send("<h1>resource not found</h1>")
