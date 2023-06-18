de64f461b42504fbe182ea5e449af600d512df87
wolfgunblood <jalajdorai1@gmail.com>
2023-06-18 17:18:36 +0530

    Params Query

--- 2-express/final/07-params-query.js
+++ 2-express/final/07-params-query.js
@@ -27,6 +27,32 @@ app.get('/api/products/:productID', (req, res) => {
     return res.json(singleProduct);
 })
 
+app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
+    console.log(req.params)
+    res.send('hello world')
+  })
+
+  app.get('/api/v1/query', (req, res) => {
+    console.log(req.query)
+    const { search, limit } = req.query;
+    let sortedProducts = [...products];
+
+    if (search) {
+      sortedProducts = sortedProducts.filter((product) => {
+        return product.name.startsWith(search);
+      });
+    }
+    if (limit) {
+      sortedProducts = sortedProducts.slice(0, Number(limit));
+    }
+    if (sortedProducts.length < 1) {
+      // res.status(200).send('no products matched your search');
+      return res.status(200).json({ success: true, data: [] });
+    }
+    res.status(200).json(sortedProducts);
+  
+  })
+
 app.all('*', (req, res) => {
     res.status(404).send("<h1>resource not found</h1>")
 })
