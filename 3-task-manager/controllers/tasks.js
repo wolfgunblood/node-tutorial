40eb7ddce5aff50586abedef24716c3550450a4d
wolfgunblood <jalajdorai1@gmail.com>
2023-07-08 11:59:10 +0530

    Params Postman

--- 3-task-manager/controllers/tasks.js
+++ 3-task-manager/controllers/tasks.js
@@ -2,10 +2,10 @@ const getAllTasks = (req, res) => {
     res.send('all items from the file');
 }
 const createTask = (req, res) => {
-    res.send('create task');
+    res.json(req.body);
 }
 const getTask = (req, res) => {
-    res.send('get single task');
+    res.json({id: req.params.id});
 }
 const updateTask = (req, res) => {
     res.send('update Task');
