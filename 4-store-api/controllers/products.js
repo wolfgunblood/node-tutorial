1183811c03e97741515b041d5c3ba9f56c606f60
wolfgunblood <jalajdorai1@gmail.com>
2023-07-08 18:10:11 +0530

    Get all Data

--- 4-store-api/controllers/products.js
+++ 4-store-api/controllers/products.js
@@ -1,9 +1,17 @@
+const Product = require('../models/product');
+
 const getAllProductsStatic = async (req, res) => {
-    throw new Error('testing async errors')
+    // throw new Error('testing async errors')
+    const products = await Product.find({    
+    });
+    res.status(201).json({ products, nbHits:products.length });
 }
 
 const getAllProducts = async (req, res) => {
-    res.status(200).json({msg: 'products route'})
+    // res.status(200).json({msg: 'products route'})
+
+    const products = await Product.find({});
+    res.status(201).json({ products });
 }
 
 module.exports = {
