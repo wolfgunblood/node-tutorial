0fac9addbb90dc5a07185e71a003ab63be181c2a
wolfgunblood <jalajdorai1@gmail.com>
2023-07-08 13:04:00 +0530

    Controllers delete and find One

--- 3-task-manager/controllers/tasks.js
+++ 3-task-manager/controllers/tasks.js
@@ -1,7 +1,15 @@
 const Task = require("../models/Task");
 
-const getAllTasks = (req, res) => {
-    res.send('all items from the file');
+const getAllTasks = async (req, res) => {
+
+    try {
+        const tasks = await Task.find({});
+        res.status(201).json({ tasks });
+
+    } catch (error) {
+        res.status(500).json({ msg: error });
+
+    }
 }
 const createTask = async (req, res) => {
     try {
@@ -13,20 +21,46 @@ const createTask = async (req, res) => {
 
     }
 }
-    const getTask = (req, res) => {
-        res.json({ id: req.params.id });
-    }
-    const updateTask = (req, res) => {
-        res.send('update Task');
-    }
-    const deleteTask = (req, res) => {
-        res.send('delete Task');
-    }
+const getTask = async (req, res) => {
+    try {
+        const { id: taskID } = req.params;
+        const task = await Task.findOne({ _id: taskID });
+        // console.log(task)
+        if (!task) {
+            return res.status(404).json({ msg: `No task with id: ${taskID}` });
+        }
+        
+        res.status(201).json({ task });
 
-    module.exports = {
-        getAllTasks,
-        createTask,
-        getTask,
-        updateTask,
-        deleteTask
+    } catch (error) {
+        res.status(500).json({ msg: error });
+
     }
+}
+const updateTask = (req, res) => {
+    res.send('update Task');
+}
+const deleteTask = async (req, res) => {
+    try {
+        const { id: taskID } = req.params;
+        const task = await Task.findOneAndDelete({ _id: taskID });
+        // console.log(task)
+        if (!task) {
+            return res.status(404).json({ msg: `No task with id: ${taskID}` });
+        }
+        
+        res.status(201).json({ task });
+
+    } catch (error) {
+        res.status(500).json({ msg: error });
+
+    }
+}
+
+module.exports = {
+    getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask
+}
