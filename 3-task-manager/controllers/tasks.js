3c5fa0533658222115337032ebf683182a82c0db
wolfgunblood <jalajdorai1@gmail.com>
2023-07-08 13:19:57 +0530

    Update Request added

--- 3-task-manager/controllers/tasks.js
+++ 3-task-manager/controllers/tasks.js
@@ -37,8 +37,23 @@ const getTask = async (req, res) => {
 
     }
 }
-const updateTask = (req, res) => {
-    res.send('update Task');
+const updateTask = async (req, res) => {
+    try {
+        const { id: taskID } = req.params;
+        const task = await Task.findOneAndUpdate({ _id: taskID }, req.body,{
+            new : true,
+            runValidators: true
+        });
+        if (!task) {
+            return res.status(404).json({ msg: `No task with id: ${taskID}` });
+        }
+        
+        res.status(201).json({task});
+
+    } catch (error) {
+        res.status(500).json({ msg: error });
+
+    }
 }
 const deleteTask = async (req, res) => {
     try {
