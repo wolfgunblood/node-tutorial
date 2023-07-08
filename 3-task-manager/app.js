a41061d78546afa2963f139c7f57bb0fdc081534
wolfgunblood <jalajdorai1@gmail.com>
2023-07-08 12:16:16 +0530

    Added Mongoose Setup

--- 3-task-manager/app.js
+++ 3-task-manager/app.js
@@ -16,18 +16,18 @@ app.get('/hello', (req, res) => {
 
 app.use("/api/v1/tasks",tasks);
 
-app.listen(3000, () => console.log("Server is listening on port 3000"));
+// app.listen(3000, () => console.log("Server is listening on port 3000"));
 
-// const start = async () => {
+const start = async () => {
 
-//     try{
-//         await connectDB(process.env.MONGO_URI);
-//         app.listen(3000, () => 
-//         console.log("Server is listening on port 3000"));
+    try{
+        await connectDB(process.env.MONGO_URI);
+        app.listen(3000, () => 
+        console.log("Server is listening on port 3000"));
 
-//     } catch (error) {
-//         console.log(error)
-//     }
-// }
+    } catch (error) {
+        console.log(error)
+    }
+}
 
-// start();
+start();
