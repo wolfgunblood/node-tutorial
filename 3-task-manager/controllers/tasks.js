84f0e3cb27349ae6d4af9cdf5a73b6b1fd697db9
wolfgunblood <jalajdorai1@gmail.com>
2023-07-08 12:34:59 +0530

    Task VAlidation Error

--- 3-task-manager/controllers/tasks.js
+++ 3-task-manager/controllers/tasks.js
@@ -4,23 +4,29 @@ const getAllTasks = (req, res) => {
     res.send('all items from the file');
 }
 const createTask = async (req, res) => {
-    const task = await Task.create(req.body);
-    res.status(201).json({task});
-}
-const getTask = (req, res) => {
-    res.json({id: req.params.id});
-}
-const updateTask = (req, res) => {
-    res.send('update Task');
-}
-const deleteTask = (req, res) => {
-    res.send('delete Task');
-}
+    try {
+        const task = await Task.create(req.body);
+        res.status(201).json({ task });
 
-module.exports = {
-    getAllTasks,
-    createTask,
-    getTask,
-    updateTask,
-    deleteTask
+    } catch (error) {
+        res.status(500).json({ msg: error });
+
+    }
 }
+    const getTask = (req, res) => {
+        res.json({ id: req.params.id });
+    }
+    const updateTask = (req, res) => {
+        res.send('update Task');
+    }
+    const deleteTask = (req, res) => {
+        res.send('delete Task');
+    }
+
+    module.exports = {
+        getAllTasks,
+        createTask,
+        getTask,
+        updateTask,
+        deleteTask
+    }
