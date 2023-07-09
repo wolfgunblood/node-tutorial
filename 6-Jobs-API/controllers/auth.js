711c181e06049c9dd144675e5dd6d44da01d06c7
wolfgunblood <jalajdorai1@gmail.com>
2023-07-09 13:25:41 +0530

    Used Jwt Token

--- 6-Jobs-API/controllers/auth.js
+++ 6-Jobs-API/controllers/auth.js
@@ -9,15 +9,9 @@ const register = async (req, res) => {
     //     throw new BadRequestError("please provide name, email and password")
     // }
 
-    const {name, email, password} = req.body
-
-    const salt = await bcrypt.genSalt(10)
-    const hashedPassword = await bcrypt.hash(password, salt)
-
-    const tempUser = {name, email, password: hashedPassword}
-
-    const user = await User.create({...tempUser})
-    res.status(StatusCodes.CREATED).json({user})
+    const user = await User.create({...req.body})
+    const token = user.createJWT()
+    res.status(StatusCodes.CREATED).json({user: {name:user.name},token})
 }
 
 const login = async (req, res) => {
