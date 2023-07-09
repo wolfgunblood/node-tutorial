54077eeff2bdb90ca5d70fdd1b95b72480eafebb
wolfgunblood <jalajdorai1@gmail.com>
2023-07-09 17:06:38 +0530

    Auth Rooute

--- 6-Jobs-API/app.js
+++ 6-Jobs-API/app.js
@@ -6,6 +6,7 @@ const connectDB = require('./db/connect');
 
 const authRouter = require("./routes/auth")
 const jobsRouter = require("./routes/jobs")
+const authenticateUser = require("./middleware/authentication")
 
 // extra security packages
 
@@ -22,7 +23,7 @@ app.get('/', (req, res) => {
 });
 
 app.use("/api/v1/auth", authRouter)
-app.use("/api/v1/jobs", jobsRouter)
+app.use("/api/v1/jobs", authenticateUser,jobsRouter)
 
 
 app.use(notFoundMiddleware);
