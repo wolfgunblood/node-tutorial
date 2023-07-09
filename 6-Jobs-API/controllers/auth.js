1addc9aebf9a009e470996176301711706bbbc45
wolfgunblood <jalajdorai1@gmail.com>
2023-07-09 17:04:19 +0530

    Addded Auth Middleware and login setup

--- 6-Jobs-API/controllers/auth.js
+++ 6-Jobs-API/controllers/auth.js
@@ -15,7 +15,22 @@ const register = async (req, res) => {
 }
 
 const login = async (req, res) => {
-    res.send("login user")
+    const { email, password } = req.body
+    if (!email || !password) {
+        throw new BadRequestError("please provide email and password")
+    }
+    const user = await User.findOne({ email })
+    if (!user) {
+        throw new UnauthenticatedError("Invalid Credentials")
+    }
+    const isPasswordCorrect = await user.comparePassword(password)
+    if (!isPasswordCorrect) {
+        throw new UnauthenticatedError("Invalid Credentials")
+    }
+    const token = user.createJWT()
+    res.status(StatusCodes.OK).json({ user: { name: user.name }, token })
+
+
 }
 
 module.exports = {
