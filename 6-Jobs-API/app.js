6bb7b79818d7e2f08b93a9ec31f36b1dfe0c9eda
wolfgunblood <jalajdorai1@gmail.com>
2023-07-09 12:38:20 +0530

    Connected MongoDB

--- 6-Jobs-API/app.js
+++ 6-Jobs-API/app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
 const app = express();
+const connectDB = require('./db/connect');
 
 const authRouter = require("./routes/auth")
 const jobsRouter = require("./routes/jobs")
@@ -31,6 +32,13 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI);
+    console.log('connected to db');
+    
+    // start server
+    console.log('starting server');
+    
+    // listen to port
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
