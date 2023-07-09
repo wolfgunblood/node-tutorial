735091d3b5dee35c5194c8be4de723c9f7823b70
wolfgunblood <jalajdorai1@gmail.com>
2023-07-09 12:33:21 +0530

    Connected Routes to App.js

--- 6-Jobs-API/app.js
+++ 6-Jobs-API/app.js
@@ -3,6 +3,11 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+const authRouter = require("./routes/auth")
+const jobsRouter = require("./routes/jobs")
+
+// extra security packages
+
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
@@ -14,6 +19,10 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('jobs api');
 });
+
+app.use("/api/v1/auth", authRouter)
+app.use("/api/v1/jobs", jobsRouter)
+
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
